48dcf28c9462c1c7278547b7967988ff21ea73cd
PRIYANKAS3110 <priyankasekarjaya@gmail.com>
2024-12-11 19:10:48 +0530

    two

--- vite.config.js
+++ vite.config.js
@@ -5,19 +5,5 @@ import { fileURLToPath } from 'node:url';
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: [
-        // Externalize Node.js core modules
-        ...builtinModules,
-        // Externalize specific libraries
-        'some-externally-required-library',
-        // Externalize a specific file by URL
-        fileURLToPath(
-          new URL('./src/some-local-file.js', import.meta.url)
-        ),
-        
-      ],
-    },
-  },
+  
 });
