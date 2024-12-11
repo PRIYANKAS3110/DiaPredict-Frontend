6ceb7807dfef3339bb6c218548bf4a36482edb20
PRIYANKAS3110 <priyankasekarjaya@gmail.com>
2024-12-11 19:14:47 +0530

    three

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { builtinModules } from 'node:module';
-import { fileURLToPath } from 'node:url';
 
 export default defineConfig({
   plugins: [react()],
-  
+  build: {
+    outDir: 'dist',
+  },
 });
