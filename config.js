5e75fd88758f4b50e6699144f12c0d826302b2e2
PRIYANKAS3110 <priyankasekarjaya@gmail.com>
2024-12-12 09:48:38 +0530

    ten

---
+++ config.js
@@ -0,0 +1,4 @@
+// src/config.js or src/apiConfig.js
+const API_BASE_URL = "https://diapredict-backend-qjtn.onrender.com";
+
+export default API_BASE_URL;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import API_BASE_URL from './config';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+fetch(`${API_BASE_URL}/api/resource`)
+  .then(response => response.json())
+  .then(data => console.log(data));
